6c550585ce671a0768214278c927e41712b40ace
Cyrus998 <jonathanfranciscosopro@gmail.com>
2022-07-27 01:42:33 +0800

    Implemented restricted access to admin pages for regular users

--- resources/views/livewire/rent-index.blade.php
+++ resources/views/livewire/rent-index.blade.php
@@ -1,6 +1,9 @@
 
 
+
 <div class="max-w-full mx-auto">
+    
+@if(Auth::user()->role=="1")
     <div class="m-2 sm:px-6 lg:px-10 text-left pt-5">
         <x-jet-button wire:click="showRentModalAdmin" class="text-1xl">Create New Rent</x-jet-button>
     </div>
@@ -73,6 +76,8 @@
         </div>
 
     </div>
+
+    @endif
     <div>
         <x-jet-dialog-modal wire:model="showingRentModal">
             @if ($isEditMode)
@@ -172,3 +177,21 @@
         </x-jet-dialog-modal>
     </div>
 </div>
+
+
+@if(Auth::user()->role=="0")
+<div class="flex mt-5 w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div class="flex items-center justify-center w-12 bg-blue-500">
+            <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z"/>
+            </svg>
+        </div>
+        
+        <div class="px-4 py-2 -mx-3">
+            <div class="mx-3">
+                <span class="font-semibold text-blue-500 dark:text-blue-400">Restricted Access</span>
+                <p class="text-sm text-gray-600 dark:text-gray-200">This account does not have administrative rights!</p>
+            </div>
+        </div>
+    </div>
+@endif
